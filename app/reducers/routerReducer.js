81d0b0cf9548dc9908ff76e713bd937e308a19ee
luke.park <luke.park@kakaocorp.com>
2016-09-11 19:21:37 +0900

    routerReducer 주석 추가

--- app/reducers/routerReducer.js
+++ app/reducers/routerReducer.js
@@ -1,5 +1,11 @@
+/*
+* Created by luke.park on 9/11/16.
+* react-router-redux에 routeReducer가 있지만 immutable하지 않기 때문에 redux-immutable로 인해서 재구현
+*/
+
 import { LOCATION_CHANGE } from 'react-router-redux'
 import { fromJS } from 'immutable'
+
 
 // Initial routing state
 const routeInitialState = fromJS({
