3253f4a617e5247b9798664583249aaa436e0bd5
luke.park <luke.park@kakaocorp.com>
2016-09-11 19:09:07 +0900

    기본 컴포넌트 추가 : Header, Menu, Popup

---
+++ app/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+/**
+ * Created by luke.park on 9/11/16.
+ */
+import React  from 'react'
+import {Link} from 'react-router'
+
+import debug from 'debug'
+const log = debug('application:Header.jsx')
+
+
+export default class Header extends React.Component {
+  render() {
+  	return (<header>
+  	header
+  	</header>)
+  }
+}
+
+Header.contextTypes = {
+  router: React.PropTypes.object.isRequired,
+  store: React.PropTypes.object.isRequired
+}
