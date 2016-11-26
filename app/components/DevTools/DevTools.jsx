149919496b2abaa68c00812d8ec6225af3471f47
luke.park <luke.park@kakaocorp.com>
2016-11-27 02:05:54 +0900

    time travel debugging tool

--- app/components/DevTools/DevTools.jsx
+++ app/components/DevTools/DevTools.jsx
@@ -5,12 +5,15 @@ import React from 'react'
 import { createDevTools } from 'redux-devtools'
 import LogMonitor from 'redux-devtools-log-monitor'
 import DockMonitor from 'redux-devtools-dock-monitor'
+//import SliderMonitor from 'redux-slider-monitor'
 
 export default createDevTools(
   <DockMonitor toggleVisibilityKey="ctrl-h"
                changeMonitorKey="ctrl-m"
                defaultPosition="right"
                changePositionKey="ctrl-w">
+
     <LogMonitor />
+{/*    <SliderMonitor keyboardEnabled />*/}
   </DockMonitor>
 )
